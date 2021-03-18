771e3490b1aa7aa1a854cef2c5948273fd8ff0d8
caiohenrique-developer <caiorussellrox@msn.com>
2021-03-18 17:39:23 -0300

    :sparkles: [ FEAT ]: Challenge done

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,6 +15,7 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
+    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if (!newTaskTitle) return;
 
     const createTask = {
@@ -29,10 +30,19 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const checkTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(checkTask);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(task => task.id !== id);
+
+    setTasks(removeTask);
   }
 
   return (
@@ -75,7 +85,6 @@ export function TaskList() {
               </button>
             </li>
           ))}
-          
         </ul>
       </main>
     </section>
